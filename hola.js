80c2e163b61413503ab6e0c4d8320196bd3acdc5
Luis Nava <luiseduardo@Felipes-MacBook-Pro.local>
2023-02-09 07:16:53 -0600

    Trabajo en clase

---
+++ hola.js
@@ -0,0 +1,4 @@
+let grados = require("celsius-fahrenheit-converter");   //dependencia
+
+console.log(grados.celsiusToFahrenheit(30));    //prueba celsius
+console.log(grados.fahrenheitToCelcius(140));      //prueba fahrenheit
